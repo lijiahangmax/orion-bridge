fd8d63b6634d4f6651caa60c3ea70b8eac4176f6
lijiahang <ljh1553488six@139.com>
2022-10-12 21:40:45 +0800

    init commit.

---
+++ orion-bridge-api/orion-bridge-mapping/src/main/java/com/orion/bridge/App.java
@@ -0,0 +1,13 @@
+package com.orion.bridge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
